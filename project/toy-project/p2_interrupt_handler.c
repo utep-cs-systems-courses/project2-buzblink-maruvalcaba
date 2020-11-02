79fadc43bb6809d21ad7d4cddf8c40b00db8f8f2
maruvalcaba <maruvalcaba@miners.utep.edu>
2020-11-01 20:38:32 -0700

    Made an edit to the p2_interrupt_handler.c

--- project/toy-project/p2_interrupt_handler.c
+++ project/toy-project/p2_interrupt_handler.c
@@ -4,21 +4,9 @@
 /* Switch on P2 (SW1-4) */
 void
 __interrupt_vec(PORT2_VECTOR) Port_1(){
-  if (P2IFG & SW1) {	      /* did a button cause this interrupt? */
-    P2IFG &= ~SW1;		      /* clear pending sw interrupts */
+  if (P2IFG & SWITCHES) {	      /* did a button cause this interrupt? */
+    P2IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
     switch_interrupt_handler();
-  }
-  if (P2IFG & SW2) {	      /* did a button cause this interrupt? */
-    P2IFG &= ~SW2;		      /* clear pending sw interrupts */
-    switch_interrupt_handler();
-  }
-  if (P2IFG & SW3) {	      /* did a button cause this interrupt? */
-    P2IFG &= ~SW3;		      /* clear pending sw interrupts */
-    switch_interrupt_handler();	
-  }
-  if (P2IFG & SW4) {	      /* did a button cause this interrupt? */
-    P2IFG &= ~SW4;		      /* clear pending sw interrupts */
-    switch_interrupt_handler();	
   }
 }
 
